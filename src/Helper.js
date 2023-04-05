7ba0e6046e83e1a07c0f1b13527973075a32ba83
thea785 <theawallnc@gmail.com>
2023-04-05 10:57:20 -0400

    Created helper for random indexes

---
+++ src/Helper.js
@@ -0,0 +1,32 @@
+const getStanzaOrder = function(numberOfStanzas) {
+    const array = [];
+    for (let i = 0; i < numberOfStanzas; i++) {
+        array.push(i);
+      }
+
+    return shuffle(array);
+}
+// Shuffle Algorithm found at:
+// stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffle(array) {
+  let currentIndex = array.length,
+    randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+}
+
+// Update the parameter as json data grows
+export const stanzaOrder = getStanzaOrder(3);
