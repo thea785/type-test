e25f3c4ff4f831946dfec6cae470cc294dc8d2e1
thea785 <theawallnc@gmail.com>
2023-04-04 21:12:55 -0400

    Added keyboard input

--- src/App.js
+++ src/App.js
@@ -3,11 +3,26 @@ import React, { useState, useEffect } from "react";
 
 function App() {
   const [items, setItems] = useState([]);
+  const [input, setInput] = useState("");
 
   useEffect(() => {
     fetch("/type-test/data.json")
       .then((response) => response.json())
       .then((jsonData) => setItems(jsonData.items));
+  }, []);
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.key === "Backspace") {
+        setInput((prevInput) => prevInput.slice(0, -1));
+      } else if (event.key.length === 1) {
+        setInput((prevInput) => prevInput + event.key);
+      }
+    };
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
   }, []);
 
   return (
@@ -19,6 +34,7 @@ function App() {
             <li key={index}>{item}</li>
           ))}
         </ul>
+        <p>{input}</p>
       </header>
     </div>
   );
